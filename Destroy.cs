ad51bf39cc4e4bae0f3c1896fadbad564882c060
GyperDranabob <148706377+GyperDranabob@users.noreply.github.com>
2023-10-22 18:33:06 +0300

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// This code is best of the best
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
